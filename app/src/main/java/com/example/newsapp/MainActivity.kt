a5cc5a31e40449cade3f50921f6537ad3768d445
KevinCarm <kevincarm016@hotmail.com>
2023-12-21 11:07:55 -0600

    Creating a list that shows a list of news

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,22 +1,64 @@
 package com.example.newsapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newsapp.data.views.NewsRecyclerAdapter
 import com.example.newsapp.databinding.ActivityMainBinding
 import com.example.newsapp.viewmodel.NewsViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextView.OnEditorActionListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: NewsViewModel
+    private lateinit var newsAdapter: NewsRecyclerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(this.binding.root)
 
         viewModel = ViewModelProvider(this)[NewsViewModel::class.java]
-        //viewModel.getArticles("Apple", 20, getString(R.string.apiKey))
+        setup()
+    }
+
+    private fun setup() {
+        binding.searchInput.setOnEditorActionListener(this)
+        newsAdapter = NewsRecyclerAdapter(emptyList())
+        val manager: RecyclerView.LayoutManager = LinearLayoutManager(applicationContext)
+        binding.newList.layoutManager = manager
+        binding.newList.adapter = newsAdapter
+        viewModel.articles.observe(this) {
+            if(it.articles.isNotEmpty()) {
+                binding.progressIndicator.visibility = View.INVISIBLE
+                newsAdapter.setList(it.articles)
+            } else {
+                binding.progressIndicator.visibility = View.INVISIBLE
+            }
+        }
+    }
+
+    override fun onEditorAction(textView: TextView?, action: Int, keyEvent: KeyEvent?): Boolean {
+       if(action == EditorInfo.IME_ACTION_SEARCH) {
+           binding.progressIndicator.visibility = View.VISIBLE
+           with(binding.searchInput) {
+               if(text.toString().isNotEmpty()) {
+                   viewModel.getArticles(text.toString(), 25, getString(R.string.apiKey))
+                   hideKeyBoard()
+                   return true
+               }
+           }
+       }
+        return false
+    }
+    private fun hideKeyBoard() {
+        val keyBoard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        keyBoard.hideSoftInputFromWindow(binding.searchInput.windowToken, 0)
     }
 }
---
+++ app/src/main/java/com/example/newsapp/data/views/NewsRecyclerAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.newsapp.data.views
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newsapp.R
+import com.example.newsapp.data.Article
+
+class NewsRecyclerAdapter(
+    private var list: List<Article>
+): RecyclerView.Adapter<NewsRecyclerAdapter.ViewHolder>() {
+
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val title: TextView
+        val author: TextView
+        val content: TextView
+
+        init {
+            title = view.findViewById(R.id.title)
+            author = view.findViewById(R.id.author)
+            content = view.findViewById(R.id.content)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.new_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+      with(list[position]) {
+          holder.title.text = title
+          holder.author.text = author
+          holder.content.text = description
+      }
+    }
+
+    fun setList(newList: List<Article>) {
+        this.list = newList
+        notifyDataSetChanged()
+    }
+}
